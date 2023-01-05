068710483ba247efbf20592b9598e15d3f1f7587
Kheir Eddine <khairi.informatique@gmail.com>
2023-01-05 22:50:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -386,8 +386,8 @@ def greeter1():
 @app.route("/calculesent", methods=['POST', 'GET'])
 def greeter2():
 	if request.form['action'] == '😻 Get Sentiment ☹️':
-		##v,s=sent(request.form['name_input1'])
-                ##c="The Sentiment Expressed In "+str(request.form['name_input1'])+"\nis :"+str(s)+"\nThe Sentiment score is : "+str(v)
+                v,s=sent(request.form['name_input1'])
+                c="The Sentiment Expressed In "+str(request.form['name_input1'])+"\nis :"+str(s)+"\nThe Sentiment score is : "+str(v)
                 return render_template("sentimentoftext.html",v1=c)
         elif request.form['action'] == ' Topic Classification ':
                 v,s=topic(request.form['name_input1'])
